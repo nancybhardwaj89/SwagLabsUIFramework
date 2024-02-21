f59dfba47489df68e83d0cf3be3793bc2fc38584
Nancy <er.nancybhardwaj@gmail.com>
2024-02-21 17:58:16 +0530

    Changes in Driver Factory and Options Manager, added code for remote driver

--- src/main/java/swaglabs/factory/DriverFactory.java
+++ src/main/java/swaglabs/factory/DriverFactory.java
@@ -4,18 +4,22 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.Properties;
+import java.net.URL;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.io.FileHandler;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class DriverFactory {
 	public WebDriver driver;
 	public static String highlight;
 	private OptionsManager optionsManager;
+	Properties prop;
 	public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<WebDriver>();
 
 // This method is used to intilize the driver
@@ -25,9 +29,17 @@ public class DriverFactory {
 		highlight = prop.getProperty("highlight");
 		System.out.println("Brower Name is:" + browserName);
 		if (browserName.equalsIgnoreCase("chrome")) {
-			tlDriver.set(new ChromeDriver(optionsManager.getchromeOptions()));
+			if (Boolean.parseBoolean(prop.getProperty("remote"))) {
+				init_remoteDriver(browserName);
+			} else {
+				tlDriver.set(new ChromeDriver(optionsManager.getchromeOptions()));
+			}
 		} else if (browserName.equalsIgnoreCase("firefox")) {
-			tlDriver.set(new FirefoxDriver(optionsManager.getfirefoxOptions()));
+			if (Boolean.parseBoolean(prop.getProperty("remote"))) {
+				init_remoteDriver("firefox");
+			} else {
+				tlDriver.set(new FirefoxDriver(optionsManager.getfirefoxOptions()));
+			}
 		} else {
 			System.out.println("Please pass correct browser Name:" + browserName);
 		}
@@ -37,17 +49,37 @@ public class DriverFactory {
 		return getDriver();
 	}
 
+	private void init_remoteDriver(String browserName) {
+		System.out.println("Running test on remote browser:" + browserName);
+		try {
+			switch (browserName.toLowerCase().trim()) {
+			case "chrome":
+				tlDriver.set(
+						new RemoteWebDriver(new URL(prop.getProperty("huburl")), optionsManager.getchromeOptions()));
+				break;
+			case "firefox":
+				tlDriver.set(
+						new RemoteWebDriver(new URL(prop.getProperty("huburl")), optionsManager.getfirefoxOptions()));
+				break;
+			default:
+				System.out.println("wrong browser info..can not run on grid remote machine....");
+				break;
+			}
+		} catch (MalformedURLException e) {
+		}
+	}
+
 	public static WebDriver getDriver() {
 		return tlDriver.get();
 	}
 
 	public Properties initProperties() {
-		Properties prop = null;
 		FileInputStream ip = null;
+		prop = new Properties();
 		String env = System.getProperty("env"); // will be passing through maven as mvn clean install -Denv
 		try {
 			if (env == null) {
-				System.out.println("Running on PROD environment:" + env);
+				System.out.println("Running on PROD environment as no envrionment is specified");
 
 				ip = new FileInputStream("./swaglabs.resources/resources/config.properties");
 			} else {
@@ -67,7 +99,7 @@ public class DriverFactory {
 			e.printStackTrace();
 		}
 		try {
-			prop = new Properties();
+			// prop = new Properties();
 			prop.load(ip);
 		} catch (IOException e) {
 			e.printStackTrace();
--- src/main/java/swaglabs/factory/OptionsManager.java
+++ src/main/java/swaglabs/factory/OptionsManager.java
@@ -15,24 +15,25 @@ public class OptionsManager {
 
 	public ChromeOptions getchromeOptions() {
 		co = new ChromeOptions();
-		if (Boolean.parseBoolean(prop.getProperty("headless").trim())) {
-			co.addArguments("--headless");
-		}
-		if (Boolean.parseBoolean(prop.getProperty("incognito").trim())) {
-			co.addArguments("--incognito");
+		if (Boolean.parseBoolean(prop.getProperty("headless").trim()))co.addArguments("--headless");
+		
+		if (Boolean.parseBoolean(prop.getProperty("incognito").trim()))co.addArguments("--incognito");
+		
+		if(Boolean.parseBoolean(prop.getProperty("remote"))) {
+			co.setCapability("browserName", "chrome");
+			//co.setBrowserVersion(prop.getProperty("browserversion").trim());
 		}
 		return co;
 	}
 
 	public FirefoxOptions getfirefoxOptions() {
 		fo = new FirefoxOptions();
-		if (Boolean.parseBoolean(prop.getProperty("headless").trim())) {
-			co.addArguments("--headless");
-		}
-		if (Boolean.parseBoolean(prop.getProperty("incognito").trim())) {
-			co.addArguments("--incognito");
-
-		}
+		if (Boolean.parseBoolean(prop.getProperty("headless").trim()))co.addArguments("--headless");
+		if (Boolean.parseBoolean(prop.getProperty("incognito").trim()))co.addArguments("--incognito");
+		if(Boolean.parseBoolean(prop.getProperty("remote"))) {
+			fo.setCapability("browserName", "firefox");
+			//fo.setBrowserVersion(prop.getProperty("browserversion").trim());
+	}
 		return fo;
 	}
 }
